0d86ad90b2a48e1ae448eae9d3a62246ad8a7451
Mymlw Lau-zzz <zzzzzzzlmy@users.noreply.github.com>
2019-01-03 23:21:05 +0800

    Update 1002.c

--- 1002.c
+++ 1002.c
@@ -18,22 +18,22 @@ yi san wu
 
 int main()
 {
-  int sum=0;
-  int i;
-  char ch,s[4];                                              
-  char b[10][5]={"ling", "yi", "er", "san", "si", "wu", "liu", "qi", "ba", "jiu"};
-  while ((ch = getchar()) != '\n')
+    int sum=0;
+    int i;
+    char ch,s[4];                                              
+    char b[10][5]={"ling", "yi", "er", "san", "si", "wu", "liu", "qi", "ba", "jiu"};
+    while ((ch = getchar()) != '\n')
+    {
+	sum += (ch - '0');
+    }
+    sprintf(s, "%d", sum);            
+    for (i = 0; s[i] != 0; i++)
+    {
+	if(i > 0)
 	{
-    sum += (ch - '0');
-	}
-  sprintf(s, "%d", sum);            
-  for (i = 0; s[i] != 0; i++)
-	{
-	  if(i > 0)
-	  {
 	    printf(" ");
-	  }
-	  printf("%s", b[s[i]-'0']);
-  }
-  return 0;
+	}
+	printf("%s", b[s[i]-'0']);
+    }
+    return 0;
 }
